491e9b1836b2037c4fdd9d2024977f6f593c7068
p829911 <p829911@gmail.com>
2022-01-25 21:04:31 +0900

    project setting

---
+++ src/main/java/me/p829911/taxijava/TaxiJavaApplication.java
@@ -0,0 +1,13 @@
+package me.p829911.taxijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/p829911/taxijava/TaxiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.p829911.taxijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
